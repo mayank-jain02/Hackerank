7d10af629066e4234f447d859be03087c25f4dcb
Mayank Jain <jain.mayank96@gmail.com>
2017-10-26 18:50:32 -0400

    Update javaSringTokens.java

--- javaSringTokens.java
+++ javaSringTokens.java
@@ -1,4 +1,4 @@
-Given a string s, matching the regular expression [A-Za-z !,?._'@]+,split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+/*Given a string s, matching the regular expression [A-Za-z !,?._'@]+,split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
 
 Note: You may find the String.split method helpful in completing this challenge.
 
@@ -29,7 +29,7 @@ boy
 isn
 t
 he
-
+*/
     
 import java.io.*;
 import java.util.*;
