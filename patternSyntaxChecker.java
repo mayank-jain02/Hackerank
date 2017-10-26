2fd287c7a6fca9c6d8d48621f223b2199b96dc27
Mayank Jain <jain.mayank96@gmail.com>
2017-10-26 18:51:13 -0400

    Update patternSyntaxChecker.java

--- patternSyntaxChecker.java
+++ patternSyntaxChecker.java
@@ -1,5 +1,5 @@
 
-Using Regex, we can easily match or search for patterns in a text. Before searching for a pattern, we have to specify one using some well-defined syntax.
+/*Using Regex, we can easily match or search for patterns in a text. Before searching for a pattern, we have to specify one using some well-defined syntax.
 
 In this problem, you are given a pattern. You have to check whether the syntax of the given pattern is valid.
 
@@ -23,12 +23,12 @@ Sample Output
 
 Valid
 Invalid
-Invalid
+Invalid*/
 
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Solution
+public class patternSyntaxChecker
 {
    public static void main(String[] args){
        boolean answ;
